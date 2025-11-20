4dfc8465f99856e6b4ff3202f72404d47452dc11
Zara-codes <zarawaytogo@gmail.com>
2025-11-20 09:52:30 +0545

    Logout in backend is done

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -61,3 +61,13 @@ export const login = async (req, res) => {
         return res.status(500).json({ message: `Login error` })
     }
 }
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message: "logout successful"})
+    } catch (error) {
+        console.log(`Logout error: ${error}`)
+        return res.status(500).json({ message: `LOgout error` })
+    }
+}
