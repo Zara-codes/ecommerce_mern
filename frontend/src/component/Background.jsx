eba1f87ce297a11b91cb986ac259f8f97a4e8a83
Zara-codes <zarawaytogo@gmail.com>
2025-11-23 12:53:29 +0545

    Background Component Created

--- frontend/src/component/Background.jsx
+++ frontend/src/component/Background.jsx
@@ -7,13 +7,13 @@ import background4 from "../assets/background4.jpg"
 const Background = ({ heroCount }) => {
 
     if (heroCount === 0) {
-        return <img src={background2} alt="Background Image First" className='w-[100%] h-[100%] float-left overflow-auto object-cover' />
+        return <img src={background2} alt="Background Image First" className='w-[50%] h-[100%] float-right overflow-auto object-cover' />
     } else if (heroCount === 1) {
-        return <img src={background1} alt="Background Image Second" className='w-[100%] h-[100%] float-left overflow-auto object-cover' />
+        return <img src={background1} alt="Background Image Second" className='w-[50%] h-[100%] float-right overflow-auto object-cover' />
     } else if (heroCount === 2) {
-        return <img src={background3} alt="Background Image Third" className='w-[100%] h-[100%] float-left overflow-auto object-cover' />
+        return <img src={background3} alt="Background Image Third" className='w-[50%] h-[100%] float-right overflow-auto object-cover' />
     } else if (heroCount === 3) {
-        return <img src={background4} alt = "Background Image Fourth" className='w-[100%] h-[100%] float-left overflow-auto object-cover' />
+        return <img src={background4} alt = "Background Image Fourth" className='w-[50%] h-[100%] float-right overflow-auto object-cover' />
     }
 
 }
