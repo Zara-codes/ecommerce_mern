e0a93d455ae7fc6b18afae0dc483229667846a8f
Zara-codes <zarawaytogo@gmail.com>
2025-11-24 14:38:04 +0545

    Admin Logout successfully created

--- admin/src/component/Nav.jsx
+++ admin/src/component/Nav.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom"
+import logo from "../assets/logo.png"
+import { useContext } from 'react'
+import { AuthDataContext } from '../context/AuthContext'
+import axios from "axios"
+import { adminDataContext } from '../context/AdminContext'
 
 const Nav = () => {
+  let navigate = useNavigate()
+  let {serverUrl} = useContext(AuthDataContext)
+  let {getAdmin} = useContext(adminDataContext)
+
+  const logout = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/auth/logout`, {withCredentials: true})
+      getAdmin()
+      console.log(result.data)
+      navigate('/login')
+    } catch (error) {
+      console.log(`Admin Logout error: ${error}`)
+    }
+  }
   return (
-    <div>Nav</div>
+    <div className='w-[100vw] h-[70px] bg-[#dcdbdbf8] z-10 fixed top-0 flex items-center justify-between px-[30px] overflow-x-hidden shadow-md shadow-black'>
+      <div className='w-[30%] flex items-center justify-start gap-[10px] cursor-pointer' onClick={() => NavigationHistoryEntry('/')}>
+        <img src={logo} alt="MCART Logo" className='w-[30px]' />
+        <h1 className='text-[25px] text-[black] font-sans'>MCART</h1>
+      </div>
+      <button className='text-[15px] hover:border-[2px] border-[#89daea] cursor-pointer bg-[#000000ca] py-[10px] px-[20px] rounded-2xl text-white' onClick={logout}>Logout</button>
+    </div>
   )
 }
 
--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Nav from '../component/Nav'
 
 const Home = () => {
   return (
-    <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] relative'>Home</div>
+    <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] relative'>
+      <Nav />
+    </div>
   )
 }
 
