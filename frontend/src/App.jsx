ba2b6def624984a5dbe7cdbed06fe8f75996ff4f
Zara-codes <zarawaytogo@gmail.com>
2025-11-27 16:10:33 +0545

    Related Product And Product Details Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,10 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Collections from './pages/Collections'
 import Product from './pages/Product'
+import ProductDetail from './pages/ProductDetail'
 
 function App() {
-  let {userData} = useContext(userDataContext)
+  let { userData } = useContext(userDataContext)
   let location = useLocation()
 
   return (
@@ -20,27 +21,29 @@ function App() {
       {userData && <Nav />}
       <Routes>
         <Route path='/signup' element={
-          userData ? (<Navigate to={location.state?.from || "/"} /> )
-          : (<Registration />)
-          } />
+          userData ? (<Navigate to={location.state?.from || "/"} />)
+            : (<Registration />)
+        } />
 
         <Route path='/login'
           element={
-          userData ? (<Navigate to={location.state?.from || "/"} /> )
-          : (<Login />)
+            userData ? (<Navigate to={location.state?.from || "/"} />)
+              : (<Login />)
           } />
 
-        <Route path='/' 
-          element={userData ? <Home /> : <Navigate to="/login" 
-          state={{from: location.pathname}} /> }/>
-        <Route path='/about' element={userData ? <About /> : <Navigate to="/login" 
-          state={{from: location.pathname}} /> }/>
-        <Route path='/collections' element={userData ? <Collections /> : <Navigate to="/login" 
-          state={{from: location.pathname}} /> }/>
-        <Route path='/product' element={userData ? <Product /> : <Navigate to="/login" 
-          state={{from: location.pathname}} /> }/>
-        <Route path='/contact' element={userData ? <Contact /> : <Navigate to="/login" 
-          state={{from: location.pathname}} /> }/>
+        <Route path='/'
+          element={userData ? <Home /> : <Navigate to="/login"
+            state={{ from: location.pathname }} />} />
+        <Route path='/about' element={userData ? <About /> : <Navigate to="/login"
+          state={{ from: location.pathname }} />} />
+        <Route path='/collections' element={userData ? <Collections /> : <Navigate to="/login"
+          state={{ from: location.pathname }} />} />
+        <Route path='/product' element={userData ? <Product /> : <Navigate to="/login"
+          state={{ from: location.pathname }} />} />
+        <Route path='/contact' element={userData ? <Contact /> : <Navigate to="/login"
+          state={{ from: location.pathname }} />} />
+        <Route path='/productdetail/:productId' element={userData ? <ProductDetail /> : <Navigate to="/login"
+          state={{ from: location.pathname }} />} />
       </Routes>
     </>
   )
