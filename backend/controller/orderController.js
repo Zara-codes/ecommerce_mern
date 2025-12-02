4917ceacf7e32a528ce7b6c7a1c98d818c48711b
Zara-codes <zarawaytogo@gmail.com>
2025-12-02 09:57:25 +0545

    Orders page for users is created

---
+++ backend/controller/orderController.js
@@ -0,0 +1,41 @@
+import Order from "../model/orderModel.js"
+import User from "../model/userModel.js"
+
+
+export const placeOrder = async (req, res) => {
+    try {
+        const {items, amount, address} = req.body
+        const userId = req.userId
+        const orderData = {
+            items,
+            amount,
+            userId,
+            address,
+            paymentMethod: 'COD',
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new Order(orderData)
+        await newOrder.save()
+
+        await User.findByIdAndUpdate(userId, {cartData: {}})
+
+        return res.status(201).json({message: 'Order Placed'})
+    } catch (error) {
+        console.log(`placeOrder error: ${error}`)
+        res.status(500).json({message: 'Order Place error'})       
+    }
+}
+
+
+export const userOrders = async (req, res) => {
+    try {
+        const userId = req.userId
+        const orders = await Order.find({userId})
+        return res.status(200).json(orders)
+    } catch (error) {
+        console.log(`userOrders error: ${error}`)
+        return res.status(500).json({message: "userOrders error"})
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cors from "cors"
 import userRoutes from "./routes/userRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cartRoutes from "./routes/cartRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js"
 
 dotenv.config()
 
@@ -25,6 +26,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/product", productRoutes)
 app.use("/api/cart", cartRoutes)
+app.use("/api/order", orderRoutes)
 
 app.listen(PORT, () => {
     console.log(`App is listening at port ${PORT}`)
